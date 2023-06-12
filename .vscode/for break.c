8d0d01aa807023d71422033b1beb8faa3191ffdd
YimikaD <babsyinny@gmail.com>
2023-06-12 01:40:51 +0100

    Initial commit

---
+++ .vscode/for break.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void)
+{
+    int i, a, sum = 0;
+
+    for (i = 1; i < 5; i++)
+    {
+        printf("Enter a number: ");
+        scanf("%d", &a);
+        if (a < 0)
+        break;
+
+        sum = sum + a;
+    }
+    printf("Sum = %d", sum);
+}
